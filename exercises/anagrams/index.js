58ddf84c1e50b17038d6748ce829ae154fe14dd6
Heloá Meneses <heloa.meneses@gmail.com>
2019-02-27 19:37:21 +0100

    Add solutions to sections 6, 7, 8 and 9

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,67 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const cleanStr = (str) => str.replace(/[^\w]/g, '').toLowerCase()
+  const sortStr = str => str.split('').sort().join('');
+  const strA = sortStr(cleanStr(stringA));
+  const strB = sortStr(cleanStr(stringB));
+
+  return strA === strB;
+}
+
+// Solution #1
+// const sanitize = (str) => str.replace(/[^\w]/g,'').toLowerCase();
+// const strA = sanitize(stringA);
+// const strB = sanitize(stringB);
+// if (strA.length !== strB.length) {
+//   return false;
+// }
+// const mapA = {};
+// const mapB = {};
+// for (const char of strA) {
+//   if (mapA[char]) {
+//     mapA[char] += 1;
+//   } else {
+//     mapA[char] = 1;
+//   }
+// }
+// for (const char of strB) {
+//   if (mapB[char]) {
+//     mapB[char] += 1;
+//   } else {
+//     mapB[char] = 1;
+//   }
+// }
+// for (const char in mapA) {
+//   if (mapA[char] !== mapB[char]) {
+//     return false;
+//   }
+// }
+// return true;
+
+// Solution #2
+// function buildCharMap(str) {
+//   const charMap = {};
+//   const cleanString = str.replace(/[^\w]/g, '').toLowerCase();
+//   for (const char of cleanString) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
+// function anagrams(stringA, stringB) {
+//   const mapA = buildCharMap(stringA);
+//   const mapB = buildCharMap(stringB);
+//   if (Object.keys(mapA).length !== Object.keys(mapB).length) {
+//     return false;
+//   }
+//   for (const char in mapA) {
+//     if (mapA[char] !== mapB[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
 
 module.exports = anagrams;
--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,56 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+// Solution #1
+// const map = {};
+// for (let char of str) {
+//   if (map[char]) {
+//     map[char] += 1;
+//   } else {
+//     map[char] = 1;
+//   }
+// }
+// let highest = '';
+// for (let key in map) {
+//   if (highest === '') {
+//     highest = key;
+//   } 
+//   if (map[key] > map[highest]) {
+//     highest = key;
+//   }
+// }
+// return highest;
+
+// Solution #2
+// let charMap = {};
+// for (let char of str) {
+//   charMap[char] = charMap[char] + 1 || 1;
+// }
+// let highest = '';
+// for (let char in charMap) {
+//   if (highest === '' || charMap[char] > charMap[highest]) {
+//     highest = char;
+//   }
+// }
+// return highest;
 
 module.exports = maxChar;
